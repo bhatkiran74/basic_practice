af7d537fecbd431b1229d71d9607407f2da41c0a
Kiransing Bhat <bhatkiran74@gmail.com>
2024-09-23 11:07:25 +0530

    sum of digits of number

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        int num = 123215;
-        int r = 0;
+        int num = 12345;
+
+        int sum = 0;
         int rem;
-        int val = num;
 
-        while (num!=0){
+        while (num != 0) {
             rem = num % 10;
-            r = r*10 + rem;
-            num = num/10;
+            sum = sum + rem;
+            num = num / 10;
         }
-        if (r == val){
-            System.out.println("Given number is pallindrome");
-        }else {
-            System.out.println("Given number is NOT pallindrome");
-        }
-
+        System.out.println(sum);
     }
 }
