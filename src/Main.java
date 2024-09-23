214600ee6c294263d84a1d37622f3b673b51145b
Kiransing Bhat <bhatkiran74@gmail.com>
2024-09-23 11:04:55 +0530

    pallindrome number

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,22 @@
-import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
 
-        String s = "(){}[]";
-        boolean valid = bracketsValidation(s);
-        System.out.println(valid);
+        int num = 123215;
+        int r = 0;
+        int rem;
+        int val = num;
 
-        String s2 = "(){}[[]{]";
-        boolean valid2 = bracketsValidation(s2);
-        System.out.println(valid2);
-    }
-
-    private static boolean bracketsValidation(String s) {
-        Stack<Character> stack = new Stack<>();
-
-        for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '[' || ch == '{') {
-                stack.push(ch);
-            } else if (ch == ')' && !stack.isEmpty() && stack.peek() == '(') {
-                stack.pop();
-            } else if (ch == ']' && !stack.isEmpty() && stack.peek() == '[') {
-                stack.pop();
-            } else if (ch == '}' && !stack.isEmpty() && stack.peek() == '{') {
-                stack.pop();
-            } else {
-                return false;
-            }
+        while (num!=0){
+            rem = num % 10;
+            r = r*10 + rem;
+            num = num/10;
         }
-        return stack.isEmpty();
+        if (r == val){
+            System.out.println("Given number is pallindrome");
+        }else {
+            System.out.println("Given number is NOT pallindrome");
+        }
+
     }
 }
