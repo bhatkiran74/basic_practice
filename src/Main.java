5787bed26c070f826f815ee9150aa47d214f2963
Kiransing Bhat <bhatkiran74@gmail.com>
2024-09-23 11:11:01 +0530

    fibbonaci series

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
-
-        int num = 12345;
-
-        int sum = 0;
-        int rem;
-
-        while (num != 0) {
-            rem = num % 10;
-            sum = sum + rem;
-            num = num / 10;
+        int n1 = 0, n2 = 1;
+        int n3 = 0;
+        System.out.print(n1+" "+n2+" ");
+        for (int i = 2; i < 10; i++) {
+            n3 = n1+n2;
+            System.out.print(n3+" ");
+            n1 = n2;
+            n2 = n3;
         }
-        System.out.println(sum);
     }
 }
